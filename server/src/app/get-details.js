e54c823c356322b6962d37d9e429f67602569358
Tiago R. Lammers <trlammers@gmail.com>
2019-07-02 20:04:01 -0300

    added the full path for the image

--- server/src/app/get-details.js
+++ server/src/app/get-details.js
@@ -7,5 +7,7 @@ module.exports = async (movie_id) => {
 
     const { data } = await api.get(`/movie/${movie_id}?api_key=${process.env.API_KEY}`);
 
+    data.poster_path = `${process.env.TMDB_IMAGE_STORE}${data.poster_path}`;
+
     return data;
 }
