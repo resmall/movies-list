d085d43a8dbb6f5d3d2fd25c15524669dc04b31b
Tiago R. Lammers <trlammers@gmail.com>
2019-07-03 10:48:16 -0300

    Integrated search with main page

--- client/src/pages/Movies.js
+++ client/src/pages/Movies.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import api from '../services/api';
 import Movie from '../components/Movie';
 
+import './Movies.css'
+
 class Movies extends Component {
-    state = {
-        movies: [],
-        isFetching: false,
-        page: 1
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            movies: [],
+            isFetching: false,
+            page: 1,
+            term: '',
+            isSearching: false
+        }
     }
 
     handleScroll = () => {
@@ -21,27 +29,57 @@ class Movies extends Component {
     }
 
     async componentDidMount() {
-        const response = await api.get('movies');
-        this.setState({ movies: response.data });
+        await this.index()
         window.addEventListener('scroll', this.handleScroll);
     }
 
+    index = async () => {
+        const response = await api.get('movies');
+        this.setState({ movies: response.data });
+    }
+
     componentDidUpdate() {
-        if (!this.state.isFetching) return;
-        this.fetchMoreListItems();
+        console.log('updated')
+        if (this.state.isFetching) {
+            this.fetchMoreListItems();
+        } else if (this.state.isSearching) {
+            this.fetchSearch();
+        }
     }
 
     componentWillUnmount() {
         window.removeEventListener('scroll', this.handleScroll);
     }
 
+    search = async (e) => {
+        e.preventDefault();
+        console.log('searching', this.term.value)
+        if (this.term.value) {
+            this.setState({term: this.term.value, isSearching: true});
+        }
+    }
+
+    fetchSearch = async () => {
+        const response = await api.get(`movies/search?term=${this.state.term}&page=${this.state.page}`);
+        this.setState({ movies: response.data, isSearching: false });
+    }
+
     render () {
         return (
             <div>
-                { this.state.movies.map(movie => (
-                    <Movie movie={movie} key={movie.id}/>
-                ))}
-                {this.state.isFetching && 'Fetching more list items...'}
+                <div>
+                    <form onSubmit={this.search}>
+                        <input type="text" ref={(c) => this.term = c} name="term" />
+                        <button type="button" onClick={this.search}>Search</button>
+                    </form>
+
+                    <button className="button-link" onClick={this.index}>Upcoming Movies</button>
+
+                    { this.state.movies.map(movie => (
+                        <Movie movie={movie} key={movie.id}/>
+                    ))}
+                    {this.state.isFetching && 'Fetching more list items...'}
+                </div>
             </div>
         );
     }
--- client/src/routes.js
+++ client/src/routes.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Movies from './pages/Movies';
-import Search from './pages/Search';
 import MovieDetail from './pages/MovieDetail';
 
 function Routes() {
@@ -10,7 +9,6 @@ function Routes() {
         <Switch>
             <Route path="/" exact component={Movies} />
             <Route path="/movies" exact component={Movies} />
-            <Route path="/search" component={Search} />
             <Route path="/movies/:movie_id" component={MovieDetail} />
         </Switch>
     );
