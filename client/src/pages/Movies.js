7880108c597f7257a974301c1e8317f755dbe267
Tiago R. Lammers <trlammers@gmail.com>
2019-07-02 23:09:56 -0300

    Added a rough infinite scroll

--- client/src/pages/Movies.js
+++ client/src/pages/Movies.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import api from '../services/api';
 import { Link } from 'react-router-dom'
 import Movie from '../components/Movie';
@@ -6,11 +6,34 @@ import Movie from '../components/Movie';
 class Movies extends Component {
     state = {
         movies: [],
+        isFetching: false,
+        page: 1
+    }
+
+    handleScroll = () => {
+        if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
+        let nextPage = this.state.page + 1;
+        this.setState({isFetching: true, page: nextPage});
+    }
+
+    fetchMoreListItems = async () => {
+        const response = await api.get(`movies?page=${this.state.page}`);
+        this.setState({ movies: this.state.movies.concat(response.data), isFetching: false });
     }
 
     async componentDidMount() {
         const response = await api.get('movies');
         this.setState({ movies: response.data });
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    componentDidUpdate() {
+        if (!this.state.isFetching) return;
+        this.fetchMoreListItems();
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
     }
 
     render () {
@@ -19,6 +42,7 @@ class Movies extends Component {
                 { this.state.movies.map(movie => (
                     <Movie movie={movie} />
                 ))}
+                {this.state.isFetching && 'Fetching more list items...'}
             </div>
         );
     }
