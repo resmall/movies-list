48e1dd9e73df3015144649004399bd179b5b08f2
Tiago R. Lammers <trlammers@gmail.com>
2019-07-02 21:23:07 -0300

    fixed formatting

--- server/src/app/get-genres.js
+++ server/src/app/get-genres.js
@@ -1,6 +1,6 @@
 const api = require('../services/api');
 
 module.exports = async () => {
-    let {data : {genres}} = await api.get(`/genre/movie/list?api_key=${process.env.API_KEY}`);
+    let { data : { genres } } = await api.get(`/genre/movie/list?api_key=${process.env.API_KEY}`);
     return genres;
 }
