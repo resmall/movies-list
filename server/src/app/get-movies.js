e8df883e71970bfade21e2f03c9e5105be6a3610
Tiago R. Lammers <trlammers@gmail.com>
2019-07-03 00:39:47 -0300

    improved code for reading'

--- server/src/app/get-movies.js
+++ server/src/app/get-movies.js
@@ -3,11 +3,27 @@ const cache = require('../infra/cache/redis');
 const reloadGenreCache = require('./cache-genre')
 
 module.exports = async (page) => {
-    const { data : { results } } = await api.get(`/movie/upcoming?api_key=${process.env.API_KEY}&page=${page}`);
+    const {
+        data: {
+            results
+        }
+    } = await api.get(`/movie/upcoming?api_key=${process.env.API_KEY}&page=${page}`);
 
-    let movies = await Promise.all(results.map(async ({title, poster_path, genre_ids, release_date, id}) => {
+    let movies = await Promise.all(results.map(async ({
+        title,
+        poster_path,
+        genre_ids,
+        release_date,
+        id
+    }) => {
         poster_path = `${process.env.TMDB_IMAGE_STORE}${poster_path}`;
-        return {id, title, poster_path, genre_ids, release_date};
+        return {
+            id,
+            title,
+            poster_path,
+            genre_ids,
+            release_date
+        };
     }))
 
     for (let i = 0; i < movies.length; i++) {
